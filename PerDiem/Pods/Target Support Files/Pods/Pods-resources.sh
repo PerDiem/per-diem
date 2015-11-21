ba7f8cb319c1246fdb09df5269fca5c5d12c5a06
Francisco Rojas <frojas@alumnos.utalca.cl>
2015-11-21 12:08:38 -0800

    Add DateTools pod

--- PerDiem/Pods/Target Support Files/Pods/Pods-resources.sh
+++ PerDiem/Pods/Target Support Files/Pods/Pods-resources.sh
@@ -58,9 +58,11 @@ install_resource()
   esac
 }
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_resource "DateTools/DateTools/DateTools.bundle"
   install_resource "Parse/Parse/Resources/en.lproj"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_resource "DateTools/DateTools/DateTools.bundle"
   install_resource "Parse/Parse/Resources/en.lproj"
 fi
 
