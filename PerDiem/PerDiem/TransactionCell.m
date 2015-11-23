b469fce0c3f8871cc5c67e883b5ed6add1840360
Chad Jewsbury <chadjewsbury@gmail.com>
2015-11-22 16:17:05 -0800

    Add Transaction Cell and Tableview

---
+++ PerDiem/PerDiem/TransactionCell.m
@@ -0,0 +1,23 @@
+//
+//  TransactionCell.m
+//  PerDiem
+//
+//  Created by Chad Jewsbury on 11/22/15.
+//  Copyright © 2015 PerDiem. All rights reserved.
+//
+
+#import "TransactionCell.h"
+
+@implementation TransactionCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
